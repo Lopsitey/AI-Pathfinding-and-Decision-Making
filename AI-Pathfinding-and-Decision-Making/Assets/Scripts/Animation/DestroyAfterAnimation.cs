7baa3d6e4f00bba228a1bb966041920f938b9ece
Lopsitey <jakeastles13@gmail.com>
2026-03-30 00:27:19 +0100

    Test2

--- AI-Pathfinding-and-Decision-Making/Assets/Scripts/Animation/DestroyAfterAnimation.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DestroyAfterAnimation : MonoBehaviour
-{
-    public void destroy()
-    {
-       Destroy(gameObject);
-    }
-}
--- AI-Pathfinding-and-Decision-Making/Assets/Scripts/DecisionMaking/Pickups/BasePickupEffect.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class BasePickupEffect : ScriptableObject
-{
-    public abstract void ApplyEffect(GameObject target);
-}
